c5b9214974ba82bc9fb1382a6cf3155b97f219b6
MattRyanFernandez <mryanfernandez@gmail.com>
2022-02-23 13:54:06 -0600

    Initial Deploy

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
+
+/*
+TODO
+
+Change all images to match my data
+Edit data.js to reflect current projects/testimonials/skills
+*/
+
+export default function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Skills />
+      <Projects />
+      
+      
+      <Contact />
+    </main>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,53 @@
+export const projects = [
+  {
+    title: "Schedule Generator",
+    subtitle: "Python",
+    description:
+      "Simple python console application that allows a user to input data to generate an employee schedule. Work still in progress.",
+    image: "./pylogo.png",
+    link: "https://github.com/MattRyanFernandez/ScheduleGenerator",
+  },
+  {
+    title: "C++ Multithreaded Seven-Segment Display",
+    subtitle: "C++",
+    description:
+      "C++ console application which uses multiple threads to return the seven-segment representation of integers." ,
+    image: "./cpplogo.jpeg",
+    link: "https://github.com/MattRyanFernandez/SevenSegmentDisplay",
+  },
+ 
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Cici Farfan",
+    company: "Costco Wholesale, Merch Manager",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Kristina Hill",
+    company: "Costco Wholesale, General Manager",
+  },
+
+  {
+    quote:
+      "Matthew is the most talented, hardworking, atrractive, stud of a man I've ever known. In fact it is a blessing to even live in the same proximity as him.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jose Reynozo",
+    company: "Costco Wholesale, Assistant General Manager",
+  },
+];
+
+export const skills = [
+  "C++",
+  "Python",
+  "Java",
+  "Git/Github",
+  "Unix",
+  "ReactJS",
+];
